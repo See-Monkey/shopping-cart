5083b0f99bffe84e8971aeaeccb156389d7f85a9
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-01-17 00:23:40 -0700

    Add and style ShopProduct component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,16 +1,21 @@
 import { useOutletContext } from "react-router";
 import styles from "./Shop.module.css";
-import RatingStars from "../../components/RatingStars/RatingStars.jsx";
+import Loading from "../Loading/Loading.jsx";
+import ShopProduct from "../../components/ShopProduct/ShopProduct.jsx";
 
 export default function Shop() {
 	const { loading, products } = useOutletContext();
 
-	console.log(products);
-
 	return (
 		<main className={styles.main}>
-			<div>Shop</div>
-			<RatingStars rating={4.3} />
+			{loading && <Loading />}
+			{!loading && (
+				<div className={styles.productsContainer}>
+					{products.map((product) => (
+						<ShopProduct key={product.id} item={product} />
+					))}
+				</div>
+			)}
 		</main>
 	);
 }
---
+++ src/components/ShopProduct/ShopProduct.jsx
@@ -0,0 +1,32 @@
+import RatingStars from "../RatingStars/RatingStars.jsx";
+import styles from "./ShopProduct.module.css";
+import plusIcon from "../../icons/plus-square.svg";
+import minusIcon from "../../icons/minus-square.svg";
+
+export default function ShopProduct({ item }) {
+	return (
+		<div className={styles.itemContainer}>
+			<img src={item.image} alt="" className={styles.itemImg} />
+			<h3>{item.title}</h3>
+			<div className={styles.ratingContainer}>
+				<RatingStars rating={item.rating.rate} />
+				<p>({item.rating.count})</p>
+			</div>
+			<div className={styles.priceQtyContainer}>
+				<p>${item.price.toFixed(2)}</p>
+				<div className={styles.qtyContainer}>
+					<input type="text" className={styles.itemQty} />
+					<div className={styles.itemQtyPlusMinus}>
+						<button className={styles.plusBtn}>
+							<img src={plusIcon} alt="Plus" />
+						</button>
+						<button className={styles.minusBtn}>
+							<img src={minusIcon} alt="Minus" />
+						</button>
+					</div>
+				</div>
+			</div>
+			<button className={styles.addBtn}>ADD TO CART</button>
+		</div>
+	);
+}
