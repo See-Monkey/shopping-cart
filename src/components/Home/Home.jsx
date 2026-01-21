056c254e9f7f1fd04d9d7411aa40adc7b79ed692
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-01-20 20:23:55 -0700

    Add Home content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { useOutletContext } from "react-router";
+import { Link, useOutletContext } from "react-router";
 import styles from "./Home.module.css";
 import photoOne from "../../images/caroline-badran-L06sfkcn1Ic-unsplash.jpg";
 import photoTwo from "../../images/clay-banks-Ie3fp14d_NQ-unsplash.jpg";
@@ -24,7 +24,44 @@ export default function Home() {
 	return (
 		<main className={styles.main}>
 			<ImageCarousel images={carouselPhotos} delay={6000} />
-			<ShopProduct item={randomProduct} />
+
+			<div className={styles.animatedLogo}>
+				<h1 className={styles.welcome}>Welcome</h1>
+				<div className={styles.toTheLogoContainer}>
+					<div className={styles.toTheContainer}>
+						<h2 className={styles.toThe}>to</h2>
+						<h2 className={styles.toThe}>the</h2>
+					</div>
+					<div className={styles.logoText}>
+						<h1 className={styles.logoTextLittle}>Little</h1>
+						<h1 className={styles.logoTextBig}>BIG</h1>
+						<h1 className={styles.logoTextShop}>Shop</h1>
+					</div>
+				</div>
+			</div>
+
+			<div className={styles.homeContent}>
+				<div className={styles.links}>
+					<h3>Best Prices</h3>
+					<h3 className={styles.guaranteed}>GUARANTEED</h3>
+					<p>What are you looking for today?</p>
+					<ul>
+						<li>
+							<Link to={"shop/men"}>Men's Clothing</Link>
+						</li>
+						<li>
+							<Link to={"shop/women"}>Women's Clothing</Link>
+						</li>
+						<li>
+							<Link to={"shop/jewelry"}>Jewelry</Link>
+						</li>
+						<li>
+							<Link to={"shop/electronics"}>Electronics</Link>
+						</li>
+					</ul>
+				</div>
+				<ShopProduct item={randomProduct} />
+			</div>
 		</main>
 	);
 }
