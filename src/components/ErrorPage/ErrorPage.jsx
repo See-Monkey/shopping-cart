52984121ebb2dab1996c93f64cf4121524b3b788
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-01-21 18:05:29 -0700

    Build and style ErrorPage

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,26 @@
 import styles from "./ErrorPage.module.css";
+import Header from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
+import sadKitten from "../../images/vecteezy_sad-cat-daydreaming-illustration_44183676.png";
+import { Link } from "react-router";
 
 export default function ErrorPage() {
 	return (
-		<main className={styles.main}>
-			<h1>Error</h1>
-		</main>
+		<>
+			<Header />
+			<main className={styles.main}>
+				<h1>Hmm, something isn't right</h1>
+				<div className={styles.errorContainer}>
+					<img src={sadKitten} alt="sad kitten" />
+					<p>
+						The page you're looking for doesn't seem to exist or has been moved.
+					</p>
+				</div>
+				<p>
+					Click <Link to={"/"}>here</Link> to go back to safety.
+				</p>
+			</main>
+			<Footer />
+		</>
 	);
 }
--- src/todo.md
+++ src/todo.md
@@ -1,2 +1,1 @@
-build ErrorPage
 learn page changing animations
