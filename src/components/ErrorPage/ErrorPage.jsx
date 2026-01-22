ada5d6a027be10480c1c6a8381ea8efd352e38e4
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-01-21 18:41:37 -0700

    Fix duplicate header/footer on ErrorPage

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,10 @@
 import styles from "./ErrorPage.module.css";
-import Header from "../Header/Header.jsx";
-import Footer from "../Footer/Footer.jsx";
 import sadKitten from "../../images/vecteezy_sad-cat-daydreaming-illustration_44183676.png";
 import { Link } from "react-router";
 
 export default function ErrorPage() {
 	return (
 		<>
-			<Header />
 			<main className={styles.main}>
 				<h1>Hmm, something isn't right</h1>
 				<div className={styles.errorContainer}>
@@ -20,7 +17,6 @@ export default function ErrorPage() {
 					Click <Link to={"/"}>here</Link> to go back to safety.
 				</p>
 			</main>
-			<Footer />
 		</>
 	);
 }
